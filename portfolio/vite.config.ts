544a3eb4a6c44897b2e9c5cf8f5fae0a7874e505
Shreya Nakum <shreyanakum2@gmail.com>
2025-04-08 23:33:43 -0700

    back to og

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Jasmine-Portfolio/', // Adjust this if necessary
+  base: '/', // Adjust this if necessary
   plugins: [react()],
 })
